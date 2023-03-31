298262b63561168b14cb3975daa370c8846fb7d9
Thiago Moregola <moregola@gmail.com>
2023-03-31 14:04:05 -0300

    performance test calculate

--- Program.cs
+++ Program.cs
@@ -11,33 +11,108 @@ async Task<int> Init()
 {
     Console.WriteLine($"start - {DateTime.Now}");
     int sumException = 0;
-    int lenght = 100;
+    int lenght = 100000;
+    DateTime init = DateTime.Now;
     try
     {
-        while(sumException++ < lenght)
+        Console.WriteLine($"TEST 1 Async x Sync - {DateTime.Now} --- TOTAL = {lenght}");
+        init = DateTime.Now;
+        while (sumException++ < lenght)
         {
             try
             {
                 //Console.WriteLine($"await Task.Run - {DateTime.Now}");
                 //await Task.Run(() => { Calculate(lenght); });
 
-                Console.WriteLine($"void Calculate - {DateTime.Now}");
-                Calculate(lenght);
-
-
-                Console.WriteLine($"await CalculateAsync - {DateTime.Now}");
+                //test-1
+                //Async => Void
                 CalculateAsync(lenght);
+                Calculate(lenght);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} - {sumException}");
+            }         
+           
+        }
+        Console.WriteLine($"end - {(DateTime.Now - init).TotalMilliseconds}");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"TEST 2 Sync x Async - {DateTime.Now}");
+        sumException = 0;
+        init = DateTime.Now;
+        while (sumException++ < lenght)
+        {
+            try
+            {
+                //Console.WriteLine($"await Task.Run - {DateTime.Now}");
+                //await Task.Run(() => { Calculate(lenght); });
 
-
-    
+                //test-2
+                //Void => Async
+                Calculate(lenght);
+                CalculateAsync(lenght);
             }
             catch (Exception e)
             {
                 Console.WriteLine($"{e.Message} - {sumException}");
             }
-            Console.WriteLine($"end - {DateTime.Now}");
+        }
+        Console.WriteLine($"end - {(DateTime.Now - init).TotalMilliseconds}");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"TEST 3 Async - {DateTime.Now}");
+        sumException = 0;
+        init = DateTime.Now;
+        while (sumException++ < lenght)
+        {
+            try
+            {
+                //Console.WriteLine($"await Task.Run - {DateTime.Now}");
+                //await Task.Run(() => { Calculate(lenght); });
+
+                //test-3
+                //Async => Async
+                CalculateAsync(lenght);
+                CalculateAsync(lenght);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} - {sumException}");
+            }
 
         }
+        Console.WriteLine($"end - {(DateTime.Now - init).TotalMilliseconds}");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"-----------------------");
+        Console.WriteLine($"TEST 4 Sync - {DateTime.Now}");
+        
+        sumException = 0;     
+        init = DateTime.Now;
+        while (sumException++ < lenght)
+        {
+
+
+            try
+            {
+                //Console.WriteLine($"await Task.Run - {DateTime.Now}");
+                //await Task.Run(() => { Calculate(lenght); });
+
+                //test-4
+                //Void => Void
+                Calculate(lenght);
+                Calculate(lenght);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} - {sumException}");
+            }
+        }
+
+        Console.WriteLine($"end - {(DateTime.Now - init).TotalMilliseconds}");
+
     }
     catch (Exception e)
     {
@@ -51,17 +126,15 @@ async Task<int> Init()
 
 void Calculate(int a)
 {
-    Console.WriteLine($"Calculate sync");
-    restClient = new RestClient("https://localhost:7231/connect/get-app/123");
-    RestRequest restRequest = new RestRequest();
-    restClient.Get(restRequest);
+    if (a < 10000) Calculate(a*a);
 }
 
 async void CalculateAsync(int a)
 {
-    Console.WriteLine($"Calculate AAAsync");
+    if (a < 10000) CalculateAsync(a*a);
+}
 
-    restClient = new RestClient("https://localhost:7231/connect/get-app/123");
-    RestRequest restRequest = new RestRequest();
-    await restClient.GetAsync(restRequest);
+async void CalculateAsync2(int a)
+{
+    if (a < 10000) Calculate(a*a);
 }
